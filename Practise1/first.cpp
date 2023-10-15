989c1bd82bd4696534a5402649540bcd1758e347
dkn3r <n.diachuk333@gmail.com>
2023-10-15 18:36:16 +0300

    added solution

--- Practise1/first.cpp
+++ Practise1/first.cpp
@@ -3,15 +3,21 @@
 #define _USE_MATH_DEFINES
 #include <cmath>
 using namespace std;
+
 int main()
 {   
     int a = 2;
-    double b = 200,0;
+    double b = 200.0;
     double first = 0;
     double second = 0;
     double result = 0;
-    first = 11.21 * (1 + log10(double(b) / a));
-    second = 1 - sin(double(a / b) * M_PI) * cos(double(a / b) * M_PI);
+    
+    first = 11.21 * (1 + log10(b / double(a))); 
+    double ab_over_b = double(a) / b;
+    second = 1 - sin(ab_over_b * M_PI) * cos(ab_over_b * M_PI);
     result = pow(first / second, 2.0 / 3.0);
-    cout<<result;
+    
+    cout << result;
+    
+    return 0;
 }
--- Practise1/second.cpp
+++ Practise1/second.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main() 
 {
     int a = 3;
-    double b = 0,707;
+    double b = 0.707;
     double result = 0;
     result = abs(0.5 * sin(log10(exp(b+a))*M_PI/8)+1.308*cos(log10(exp(a-b))*M_PI/8));
     cout << result;
